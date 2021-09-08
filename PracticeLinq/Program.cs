bd8c958d609b23f5f7f055323bbe20ddfd2eb8a2
Nick Stockton <phoenix76728@gmail.com>
2021-09-08 13:05:14 -0400

    third

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Games = new List <string>();
+
+            Games.Add("Minecraft");
+            Games.Add("Skyrim: Elder Scrolls V");
+            Games.Add("Halo");
+            Games.Add("Cyberpunk 2077");
+            Games.Add("Fallout 4");
+            Games.Add("Legend of Zelda: Breath of The Wild");
+            Games.Add("DarkSouls 3");
+            Games.Add("The Outer Worlds");
+            Games.Add("Star Wars Battlefront 2");
+            Games.Add("Kingdom Come Deliverence");
+            Games.Add("GreedFall");
+            Games.Add("Mordhau");
+            Games.Add("Chivalry 2");
+
+
+            var gameList = Games.OrderBy(x=>x);
+
+            foreach (var y in gameList)
+            {
+                Console.WriteLine(y);
+            }
+            Console.WriteLine("---------------------------");
+
+
+
+
+
+        }
+    }
+}
